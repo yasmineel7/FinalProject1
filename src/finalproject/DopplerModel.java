5e47e9a4038e9f3d3f2b9a94756f6d01ee1605dc
yasmineel7 <6298686@edu.vaniercollege.qc.ca>
2025-11-07 13:47:41 -0500

    changes in progress to account for refactor of entity

--- src/finalproject/DopplerModel.java
+++ src/finalproject/DopplerModel.java
@@ -10,65 +10,72 @@ import java.util.Objects;
  *
  * @author yasmi
  */
-public class DopplerModel extends Model {
+public class DopplerModel {
     
     //variables
-    private double speedOfSound; //or only velocity
     private double velocityWave;
     private Entity source;
     private Entity observer;
     private double time;
-    private double frequencySource;
-    private double frequencyObserver;
    
-      public DopplerModel(double speedOfSound,double velocityWave, Entity source, Entity observer, double time, double frequencySource, double frequencyObserver) {
-        this.speedOfSound = speedOfSound; 
+      public DopplerModel(double velocityWave, Entity source, Entity observer, double time) { 
         this.velocityWave = velocityWave;
         this.source = source;
         this.observer = observer;
         this.time = time;
-        this.frequencySource = frequencySource;
-        this.frequencyObserver = frequencyObserver;
     }
       
     public void updateObservedFrequency() {
+        boolean sourceVelocityNull = source.getVelocity() == 0;
+        boolean observerVelocityNull = observer.getVelocity() == 0;
+        boolean positionSourceLeft = source.getPosition() < observer.getPosition();
+        boolean positionSourceRight = source.getPosition() > observer.getPosition();
+        boolean sourceVelocityNegative = source.getVelocity() < 0;
+        boolean sourceVelocityPositive = source.getVelocity() > 0;
+        boolean observerVelocityNegative = observer.getVelocity() < 0;
+        boolean observerVelocityPositive = observer.getVelocity() > 0;
         //the velocity is 0 if the observer or source is stationary
         //need a starting position?? to know if going toward/away or changing place depending on the case?
         
         //calculation for the most basic case and assuming that the observer is going toward (that the source position < then observer so going toward)
-        if (source.getVelocity() == 0 && source.getPosition() < observer.getPosition()) {
-            frequencyObserver = frequencySource * ((velocityWave + observer.getVelocity()) / velocityWave);
+        if (sourceVelocityNull && positionSourceLeft) {
+            observer.setObservedFrequency(source.getSourceFrequency() * ((velocityWave + observer.getVelocity()) / velocityWave));
         }
         
         //basic case and observer is moving away
-        if (source.getVelocity() == 0 && source.getPosition() > observer.getPosition()) {
-            frequencyObserver = frequencySource * ((velocityWave - observer.getVelocity()) / velocityWave);
+        if (sourceVelocityNull && positionSourceRight) {
+            //frequencyObserver = frequencySource * ((velocityWave - observer.getVelocity()) / velocityWave);
         }
         
         //no one is moving
-        if (source.getVelocity() == 0 && observer.getVelocity() == 0) {
-            frequencyObserver = frequencySource;
+        if (sourceVelocityNull && observerVelocityNull) {
+            //frequencyObserver = frequencySource;
         }
         
         //stationary observer ans source moving toward
-        if (observer.getVelocity() == 0 && source.getPosition() < observer.getPosition()) {
-            frequencyObserver = frequencySource * (velocityWave / (velocityWave - source.getVelocity()));
+        if (observerVelocityNull && positionSourceLeft) {
+            //frequencyObserver = frequencySource * (velocityWave / (velocityWave - source.getVelocity()));
         }
         
         //stationary observer and source moving away
-        if (observer.getVelocity() == 0 && source.getPosition() > observer.getPosition()) {
-            frequencyObserver = frequencySource * (velocityWave / (velocityWave + source.getVelocity()));
+        if (observerVelocityNull && positionSourceRight) {
+            //frequencyObserver = frequencySource * (velocityWave / (velocityWave + source.getVelocity()));
         }
         
         //observer and source going toward
-        if (source.getPosition() < observer.getPosition() && !(source.getVelocity() == 0) && !(observer.getVelocity() == 0)) {
-            frequencyObserver = frequencySource * ((velocityWave + observer.getVelocity()) / (velocityWave - source.getVelocity()));
+        if (positionSourceLeft && !sourceVelocityNull && !observerVelocityNull) {
+            //frequencyObserver = frequencySource * ((velocityWave + observer.getVelocity()) / (velocityWave - source.getVelocity()));
         }
         
         //observer going toward and source going away ?????
+        if (positionSourceRight && sourceVelocityPositive && observerVelocityNegative) {
+            
+        }
+        
+        if (position sourceLeft && sourceVelocityNegative )
         //observer going away and source going toward???????
         //observer and source moving away 
-        if (source.getPosition() > observer.getPosition() && !(source.getVelocity() == 0) && !(observer.getVelocity() == 0)) {
+        if (positionSourceRight && !sourceVelocityNull&& !observerVelocityNull) {
             frequencyObserver = frequencySource * ((velocityWave - observer.getVelocity() / (velocityWave + source.getVelocity())));
         }    
     }
@@ -76,7 +83,6 @@ public class DopplerModel extends Model {
     @Override
     public int hashCode() {
         int hash = 7;
-        hash = 59 * hash + (int) (Double.doubleToLongBits(this.speedOfSound) ^ (Double.doubleToLongBits(this.speedOfSound) >>> 32));
          hash = 59 * hash + (int) (Double.doubleToLongBits(this.velocityWave) ^ (Double.doubleToLongBits(this.velocityWave) >>> 32));
         hash = 59 * hash + Objects.hashCode(this.source);
         hash = 59 * hash + Objects.hashCode(this.observer);
@@ -98,9 +104,6 @@ public class DopplerModel extends Model {
             return false;
         }
         final DopplerModel other = (DopplerModel) obj;
-        if (Double.doubleToLongBits(this.speedOfSound) != Double.doubleToLongBits(other.speedOfSound)) {
-            return false;
-        }
         if (Double.doubleToLongBits(this.velocityWave) != Double.doubleToLongBits(other.velocityWave)) {
             return false;
         }
@@ -119,16 +122,8 @@ public class DopplerModel extends Model {
         return Objects.equals(this.observer, other.observer);
     }
     
-    public double getSpeedOfSound() {
-        return speedOfSound;
-    }
-
-    public void setSpeedOfSound(double speedOfSound) {
-        this.speedOfSound = speedOfSound;
-    }
-    
      public double getVelocityWave() {
-        return speedOfSound;
+        return velocityWave;
     }
 
     public void setVelocityWave(double velocityWave) {
