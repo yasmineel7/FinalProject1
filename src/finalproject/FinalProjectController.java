1cddd3e06c1f8413a4c1fb913e28413b0b5e867c
Jacques <112639491+waffleprinter@users.noreply.github.com>
2025-11-19 11:14:33 -0500

    added charts initialization without data

--- src/finalproject/FinalProjectController.java
+++ src/finalproject/FinalProjectController.java
@@ -6,7 +6,13 @@ package finalproject;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Series;
+import javafx.scene.layout.HBox;
 
 /**
  * FXML Controller class
@@ -14,13 +20,34 @@ import javafx.fxml.Initializable;
  * @author Yasmine
  */
 public class FinalProjectController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private HBox graphHBox;
+    
+    private LineChart[] charts = new LineChart[2];
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        initializeCharts();
+    }
     
+    /**
+     * Initializes the frequency charts to the charts variable, including axis and series.
+     */
+    public void initializeCharts() {
+        for (int i = 0; i < 2; i++) {
+            NumberAxis timeAxis = new NumberAxis(); timeAxis.setLabel("Time (s)");
+            NumberAxis frequencyAxis = new NumberAxis(); frequencyAxis.setLabel("Frequency (Hz)");
+            
+            LineChart lineChart = new LineChart(timeAxis, frequencyAxis);
+            lineChart.setTitle("Frequency Observed by " + (char)(65 + i));
+            lineChart.setCreateSymbols(false);
+            lineChart.setLegendVisible(false);
+            
+            Series series = new XYChart.Series<>();
+            
+            lineChart.getData().add(series);
+            
+            graphHBox.getChildren().add(lineChart);
+        }
+    }
 }
