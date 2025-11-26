b10c0f5631e0372f2e8e267cd178db1d5a6428df
yasmineel7 <6298686@edu.vaniercollege.qc.ca>
2025-11-26 16:12:11 -0500

    started the Junit test

--- test/DopplerModelTest.java
+++ test/DopplerModelTest.java
@@ -43,5 +43,33 @@ public class DopplerModelTest {
     // @Test
     // public void hello() {}
    
-
+       @Test
+        void updateObservedFrequency() {
+         Entity observer = new Entity(10, 10, 10, 50);
+         Entity source = new Entity(15, 20, 10, 40);
+         double result = 41.238;
+         
+         DopplerModel model = new DopplerModel();
+        //expected = model.updateObservedFrequency(observer, source);
+         assertEquals(result, source.);
+        
+       }
+       
+       @Test 
+       void updateKinematicState() {
+           Entity entity = new Entity(10, 10, 10, 50);
+           double time = 5;
+           
+           double result = 310;
+           
+           DopplerModel model = new DopplerModel();
+           model.updateKinematicState(entity, time);
+           
+           assertEquals()
+       }
+       
+       @Test
+       public void update() {
+           
+       }
 }
