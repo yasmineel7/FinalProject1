d33c952286c32468324df8c50786b854481cc6f9
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-11-30 22:29:07 -0500

    added the outline for some junit tests (they dont actually work)

--- test/finalproject/FinalProjectControllerTest.java
+++ test/finalproject/FinalProjectControllerTest.java
@@ -6,7 +6,13 @@ package finalproject;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.embed.swing.JFXPanel;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,93 +22,140 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
- * @author jccpa
+ * @author Jacques
  */
 public class FinalProjectControllerTest {
+    // We tried everything we could, but Junit is not working for us.
+    // We wrote some code that gives the main idea behind Junit testing,
+    // but we are unable to actually test it out and make sure that it works.
     
-    public FinalProjectControllerTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
+    // Since none of this works, we pretended that everything is public to make
+    // making writing the fake code easier
+    FinalProjectController controller;
+
+    @Test
+    void testModelInitialization() {
+        assertNotNull(controller.model);
+        assertEquals(343, controller.model.getVelocityWave());
+        assertEquals(0, controller.model.getEntityA().getPosition());
+        assertEquals(800, controller.model.getEntityB().getPosition());
     }
 
-    /**
-     * Test of initialize method, of class FinalProjectController.
-     */
     @Test
-    public void testInitialize() {
-        System.out.println("initialize");
-        URL url = null;
-        ResourceBundle rb = null;
-        FinalProjectController instance = new FinalProjectController();
-        instance.initialize(url, rb);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    void testChartsAreCreated() {
+        assertNotNull(controller.frequencyChartA);
+        assertNotNull(controller.frequencyChartB);
+        assertNotNull(controller.waveLengthChartA);
+        assertNotNull(controller.waveLengthChartB);
+
+        assertEquals(1, controller.frequencyChartA.getData().size());
+        assertEquals(1, controller.frequencyChartB.getData().size());
     }
 
-    /**
-     * Test of handleReset method, of class FinalProjectController.
-     */
     @Test
-    public void testHandleReset() {
-        System.out.println("handleReset");
-        ActionEvent event = null;
-        FinalProjectController instance = new FinalProjectController();
-        instance.handleReset(event);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    void testGetTruckFromEntity() {
+        assertSame(controller.truckA, controller.getTruckFromEntity(controller.model.getEntityA()));
+        assertSame(controller.truckB, controller.getTruckFromEntity(controller.model.getEntityB()));
     }
 
-    /**
-     * Test of handleExit method, of class FinalProjectController.
-     */
     @Test
-    public void testHandleExit() {
-        System.out.println("handleExit");
-        ActionEvent event = null;
-        FinalProjectController instance = new FinalProjectController();
-        instance.handleExit(event);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    void testGetEntityFromTruck() {
+        assertSame(controller.model.getEntityA(), controller.getEntityFromTruck(controller.truckA));
+        assertSame(controller.model.getEntityB(), controller.getEntityFromTruck(controller.truckB));
     }
 
-    /**
-     * Test of handlePause method, of class FinalProjectController.
-     */
     @Test
-    public void testHandlePause() {
-        System.out.println("handlePause");
-        ActionEvent event = null;
-        FinalProjectController instance = new FinalProjectController();
-        instance.handlePause(event);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    void testTruckUpdatesPosition() {
+        controller.model.getEntityA().setPosition(123.0);
+        controller.updateTruck(controller.truckA);
+        assertEquals(123.0, controller.truckA.getLayoutX());
     }
 
-    /**
-     * Test of handleStart method, of class FinalProjectController.
-     */
     @Test
-    public void testHandleStart() {
-        System.out.println("handleStart");
-        ActionEvent event = null;
-        FinalProjectController instance = new FinalProjectController();
-        instance.handleStart(event);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    void testTruckFacesLeftWhenVelocityNegative() {
+        controller.model.getEntityA().setVelocity(-10);
+        controller.updateTruck(controller.truckA);
+
+        assertEquals(-1, controller.truckA.getScaleX());
     }
-    
+
+    @Test
+    void testTruckFacesRightWhenVelocityPositive() {
+        controller.model.getEntityA().setVelocity(10);
+        controller.updateTruck(controller.truckA);
+
+        assertEquals(1, controller.truckA.getScaleX());
+    }
+
+    @Test
+    void testTruckColorUpdates() {
+        controller.truckAColor = Color.RED;
+        controller.wheelsAColor = Color.BLUE;
+        controller.updateTruckColors();
+
+        assertEquals(Color.RED, controller.truckABody.getFill());
+        assertEquals(Color.BLUE, controller.truckAWheel1.getFill());
+    }
+
+    @Test
+    void testResetColors() {
+        controller.truckAColor = Color.BLACK;
+        controller.wheelsAColor = Color.YELLOW;
+
+        controller.resetColorsToDefault();
+
+        assertEquals(Color.WHITE, controller.truckAColor);
+        assertEquals(Color.WHITE, controller.wheelsAColor);
+    }
+
+    @Test
+    void testAddPointToChart() {
+        LineChart<Number, Number> chart = controller.frequencyChartA;
+        controller.addPoint(chart, 1.0, 100.0);
+
+        assertEquals(1, chart.getData().get(0).getData().size());
+        assertEquals(1.0, chart.getData().get(0).getData().get(0).getXValue());
+        assertEquals(100.0, chart.getData().get(0).getData().get(0).getYValue());
+    }
+
+    @Test
+    void testClearCharts() {
+        controller.addPoint(controller.frequencyChartA, 1, 1);
+        controller.addPoint(controller.waveLengthChartB, 2, 5);
+
+        controller.clearAllCharts();
+
+        assertEquals(0, controller.frequencyChartA.getData().get(0).getData().size());
+        assertEquals(0, controller.waveLengthChartB.getData().get(0).getData().size());
+    }
+
+    @Test
+    void testCreateSoundWaveAddsCircle() {
+        int before = controller.scenePane.getChildren().size();
+
+        controller.createSoundWave(controller.model.getEntityA(), Color.RED);
+
+        int after = controller.scenePane.getChildren().size();
+
+        assertTrue(after > before);
+    }
+
+    @Test
+    void testSoundWaveTransitionAdded() {
+        controller.createSoundWave(controller.model.getEntityA(), Color.RED);
+
+        assertFalse(controller.soundWaveTransitions.isEmpty());
+    }
+
+    @Test
+    void testResetRestoresModelValues() {
+        controller.model.getEntityA().setPosition(123);
+        controller.model.setTime(4.5);
+
+        controller.handleReset(null);
+
+        assertEquals(0, controller.model.getEntityA().getPosition());
+        assertEquals(800, controller.model.getEntityB().getPosition());
+        assertEquals(0, controller.model.getTime());
+    }
 }
